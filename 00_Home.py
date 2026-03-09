40ba01127f363ea279342e4829740e19ea2f90df
Hendrik Esser <hendrikesser@Hendriks-MacBook-Air.local>
2026-03-09 15:42:51 +0800

    Update Streamlit app: Stock & Commodity Futures section and other changes

--- 00_Home.py
+++ 00_Home.py
@@ -2,6 +2,19 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
+from streamlit.components.v1 import html
+
+html("""
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KESJH7R5DT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-KESJH7R5DT');
+</script>
+"""), height=0, width=0)
 
 # ===========================
 # Page Title & Layout
